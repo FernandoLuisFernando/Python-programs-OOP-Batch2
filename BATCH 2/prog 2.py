57bf139741f46a3a32d79f02f4aa4964dc4ae8cd
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-07 17:05:09 +0800

    third commit

---
+++ BATCH 2/prog 2.py
@@ -0,0 +1,9 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+num1 = int(input("Enter an integer: "))
+num2 = int(input("Enter an integer: "))
+if num1 != num2:
+    print("Both numbers are equal.")
+else:
+    print("Both numbers are NOT equal.")
+    
