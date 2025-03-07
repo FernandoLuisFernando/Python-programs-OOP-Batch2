1d2bfb91d962bafe5d3514d9f46c28ad13034c0a
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-07 16:59:50 +0800

    third commit

--- BATCH 2/prog 1.py
+++ BATCH 2/prog 1.py
@@ -1,3 +1,5 @@
+#Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
